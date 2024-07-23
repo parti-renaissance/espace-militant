64b9afbe5acbbc2ba53d0782bd1ae4c2f232bbae
Antonin <antonin.carlin@icloud.com>
2024-07-23 16:03:25 +0200

    ci: eslint

--- src/components/404/Error404.tsx
+++ src/components/404/Error404.tsx
@@ -1,7 +1,7 @@
 // import { Image } from 'react-native'
 import PageLayout from '@/components/layouts/PageLayout/PageLayout'
 // import { Image } from 'expo-image'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
 import { Image, View, YStack } from 'tamagui'
 import Text from '../base/Text'
 import { VoxButton } from '../Button'
