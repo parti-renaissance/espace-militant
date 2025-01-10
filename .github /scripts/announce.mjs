1b086b70ba568a7dea8764e7ecdf07fa4fe678c6
Antonin CARLIN <antonin.carlin@icloud.com>
2025-01-10 18:25:35 +0100

    new CI for 5.13.0

---
+++ .github /scripts/announce.mjs
@@ -0,0 +1,24 @@
+/// <reference types="node" />
+/* eslint-env node */
+/* global fetch, process */
+/* eslint-disable no-console */
+
+const payload = {
+  sender: 'github-actions',
+  event: process.env.WORKFLOW_JOB,
+  type: process.env.EAS_WORKFLOW_TYPE,
+  branch: process.env.BRANCH_NAME,
+  domain: process.env.EXPO_PUBLIC_ASSOCIATED_DOMAIN,
+  last_commit_message: process.env.LAST_COMMIT_MESSAGE,
+  comment: process.env.WORFLOW_COMMENT_INPUT ?? null,
+  env: process.env.WORKFLOW_ENVIRONMENT,
+  github_workflow_url: process.env.WORKFLOW_URL,
+}
+
+const requestOptions = {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify(payload),
+}
+
+fetch(process.env.MAKE_WEBHOOK_URL, requestOptions)
