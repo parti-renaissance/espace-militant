7abcaadb5404f97960f6207aec2ea349eb6b4749
Antonin CARLIN <antonin.carlin@icloud.com>
2025-01-15 11:48:21 +0100

    8697g8r92 - EXPO 52, fix expo-update

--- src/features/update/updateScreen.tsx
+++ src/features/update/updateScreen.tsx
@@ -1,29 +1,28 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
-import { AppState } from 'react-native'
+import { useCallback, useEffect, useMemo, useRef } from 'react'
+import { AppState, StatusBar } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import EuCampaignIllustration from '@/assets/illustrations/EuCampaignIllustration'
 import Text from '@/components/base/Text'
 import { VoxButton } from '@/components/Button'
 import LayoutPage from '@/components/layouts/PageLayout/PageLayout'
+import useAppUpdate from '@/features/update/hooks/useAppUpdate'
 import redirectToStore from '@/helpers/redirectToStore'
 import useAsyncFn from '@/hooks/useAsyncFn'
-import { ErrorMonitor } from '@/utils/ErrorMonitor'
 import NetInfo from '@react-native-community/netinfo'
 import { Image } from 'expo-image'
-import * as Updates from 'expo-updates'
-import { isWeb, Spinner, View, XStack, YStack } from 'tamagui'
-import { checkStoreUpdate } from './utils'
+import { reloadAsync } from 'expo-updates'
+import { getTokenValue, Spinner, View, XStack, YStack } from 'tamagui'
 
 interface Props {
   children: React.ReactNode
 }
 
-const useAppStateOnChange = (callback: () => Promise<void>) => {
+const useAppStateOnChange = (callback: () => void) => {
   const appState = useRef(AppState.currentState)
   useEffect(() => {
-    const subscription = AppState.addEventListener('change', async (nextAppState) => {
+    const subscription = AppState.addEventListener('change', (nextAppState) => {
       if (appState.current.match(/inactive|background/) && nextAppState === 'active') {
-        await callback()
+        callback()
       }
       appState.current = nextAppState
     })
@@ -35,67 +34,53 @@ const useAppStateOnChange = (callback: () => Promise<void>) => {
 }
 
 export default function UpdateScreen({ children }: Props) {
-  const [updateType, setUpdateType] = useState<'build' | 'update' | null>(null)
-  const { isDownloading } = Updates.useUpdates()
+  const { isBuildUpdateAvailable, checkForUpdate, isUpdateAvailable, isDownloading, isUpdatePending } = useAppUpdate()
+  const hasAnyUpdate = isUpdateAvailable || isBuildUpdateAvailable
+  const message = isBuildUpdateAvailable
+    ? 'Mettez à jour votre application depuis votre store pour découvrir les nouveautés.'
+    : 'Nous mettons à jour votre application en arrière plan pour vous faire profiter des dernières nouveautés.'
 
-  const message =
-    updateType === 'build'
-      ? 'Mettez à jour votre application depuis votre store pour découvrir les nouveautés.'
-      : 'Nous mettons à jour votre application en arrière plan pour vous faire profiter des dernières nouveautés.'
-
-  useAppStateOnChange(async () => {
-    if (isWeb) {
-      return
-    }
-    return NetInfo.fetch().then(async (state) => {
+  useAppStateOnChange(() => {
+    NetInfo.fetch().then((state) => {
       if (state.isConnected) {
-        const newStoreBuildAvailable = await checkStoreUpdate()
-        if (newStoreBuildAvailable) {
-          setUpdateType('build')
-        } else {
-          const isEasUpdateAvailable = await Updates.checkForUpdateAsync()
-          if (isEasUpdateAvailable) {
-            setUpdateType('update')
-            try {
-              await Updates.fetchUpdateAsync()
-              await Updates.reloadAsync()
-            } catch (e) {
-              if (e instanceof Error) {
-                ErrorMonitor.log(e.message)
-              } else {
-                ErrorMonitor.log("Un erreur lors de la mise à jour s'est produite")
-              }
-            }
-          } else {
-            setUpdateType(null)
-          }
-        }
+        checkForUpdate()
       }
     })
   })
 
   const { isProcessing, trigger: onUpdate } = useAsyncFn(
     useCallback(async () => {
-      await redirectToStore()
+      if (isBuildUpdateAvailable) {
+        await redirectToStore()
+      } else {
+        await reloadAsync()
+      }
     }, []),
   )
 
+  useEffect(() => {
+    if (!isDownloading && isUpdatePending) {
+      onUpdate()
+    }
+  }, [isDownloading, isUpdatePending])
+
   const updateStatusMessage = useMemo(() => {
-    if (updateType !== 'update') return null
+    if (!isUpdateAvailable) return null
     if (isDownloading) return 'Téléchargement...'
-    if (!isDownloading) return 'Mise à jour....'
+    if (isProcessing) return 'Mise à jour....'
     return null
-  }, [isDownloading, updateType])
+  }, [isDownloading, isProcessing, isUpdateAvailable])
 
   const insets = useSafeAreaInsets()
 
-  if (!updateType) {
+  if (!hasAnyUpdate) {
     return children
   }
 
   return (
     <LayoutPage>
-      <View height="100%" p={'$medium'} pt={insets.top} pb={insets.bottom} flex={1}>
+      <StatusBar barStyle="dark-content" />
+      <View p={'$medium'} pt={insets.top} pb={insets.bottom + getTokenValue('$medium')} flex={1}>
         <XStack alignItems="center" justifyContent="center" gap={'$large'}>
           <EuCampaignIllustration pt={'$large'} showIcon={false} />
         </XStack>
@@ -114,7 +99,7 @@ export default function UpdateScreen({ children }: Props) {
             ) : null}
           </YStack>
         </YStack>
-        {updateType === 'build' ? (
+        {isBuildUpdateAvailable ? (
           <XStack>
             <VoxButton onPress={onUpdate} theme="blue" full size="xl" loading={isProcessing}>
               Mettre à jour
