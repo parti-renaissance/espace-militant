a782a348919fafd70489b73cccc5a380d4c39d8f
Antonin CARLIN <antonin.carlin@icloud.com>
2025-02-27 12:44:40 +0100

    feat: optimize node selector

--- src/features/message/pages/create-update/components/NodeSelectorWrapper.tsx
+++ src/features/message/pages/create-update/components/NodeSelectorWrapper.tsx
@@ -1,10 +1,28 @@
-import { ReactNode, useCallback, useMemo } from 'react'
+import { memo, ReactNode, useMemo, useRef } from 'react'
 import { GestureResponderEvent } from 'react-native'
 import * as S from '@/features/message/schemas/messageBuilderSchema'
+import { useLazyRef } from '@/hooks/useLazyRef'
 import { Control, Controller } from 'react-hook-form'
-import { styled, ThemeableStack, View } from 'tamagui'
+import { createStyledContext, styled, ThemeableStack, View, withStaticProperties } from 'tamagui'
+
+const wrapperContext = createStyledContext({
+  selected: false,
+})
+
+const WrapperFrame = styled(ThemeableStack, {
+  context: wrapperContext,
+  variants: {
+    selected: {
+      true: {
+        borderRadius: 16,
+        overflow: 'hidden',
+      },
+    },
+  } as const,
+})
 
 const SelectOverlay = styled(ThemeableStack, {
+  context: wrapperContext,
   position: 'absolute',
   top: 0,
   left: 0,
@@ -13,6 +31,9 @@ const SelectOverlay = styled(ThemeableStack, {
   zIndex: 1,
   cursor: 'pointer',
   borderRadius: 16,
+  hoverStyle: {
+    backgroundColor: '$gray/24',
+  },
   variants: {
     selected: {
       true: {
@@ -25,6 +46,7 @@ const SelectOverlay = styled(ThemeableStack, {
 })
 
 const SelectOverlayLayer2 = styled(ThemeableStack, {
+  context: wrapperContext,
   flex: 1,
   width: '100%',
   borderRadius: 11,
@@ -39,33 +61,60 @@ const SelectOverlayLayer2 = styled(ThemeableStack, {
   } as const,
 })
 
+const Wrapper = withStaticProperties(WrapperFrame, {
+  Props: wrapperContext.Provider,
+  Overlay: SelectOverlay,
+  OverlayContainer: SelectOverlayLayer2,
+})
+
 type NodeSelectorProps = {
   field: S.FieldsArray[number]
   children: ReactNode
   control: Control<S.GlobalForm>
 }
 
-export const NodeSelectorWrapper = (props: NodeSelectorProps) => {
-  const content = useMemo(() => <View pointerEvents="none">{props.children}</View>, [props.children])
-  const handlePress = useCallback(
-    (fn: (x: S.FieldsArray[number]) => void) => (e: GestureResponderEvent) => {
-      e.stopPropagation()
-      fn(props.field)
-    },
-    [props.field],
-  )
+const MemoWrapper = memo((props: { selected: boolean; htmlId: string; onWrapperPress: (e: GestureResponderEvent) => void; children: ReactNode }) => {
   return (
-    <View id={`field-${props.field.type}-${props.field.id}`}>
-      <Controller
-        render={({ field }) => (
-          <SelectOverlay onPress={handlePress(field.onChange)} selected={field.value?.id === props.field.id}>
-            <SelectOverlayLayer2 selected={field.value?.id === props.field.id} />
-          </SelectOverlay>
-        )}
-        control={props.control}
-        name="selectedField"
-      />
-      {content}
-    </View>
+    <Wrapper.Props selected={props.selected}>
+      <Wrapper id={props.htmlId}>
+        <Wrapper.Overlay onPress={props.onWrapperPress}>
+          <Wrapper.OverlayContainer />
+        </Wrapper.Overlay>
+        {props.children}
+      </Wrapper>
+    </Wrapper.Props>
   )
-}
+})
+
+MemoWrapper.displayName = 'MemoWrapper'
+
+export const NodeSelectorWrapper = memo((props: NodeSelectorProps) => {
+  const content = useMemo(() => <View pointerEvents="none">{props.children}</View>, [props.children])
+  //trick to avoid rerenders, avoid to create a new function each time, props.field can't change and field.onChange is a setter, and can't change
+  const handlePress = useRef<{ fn: ((e: GestureResponderEvent) => void) | null }>({ fn: null })
+  const handlePressSetter = useLazyRef(() => (fn: (x: S.FieldsArray[number]) => void) => {
+    if (handlePress.current.fn === null) {
+      handlePress.current.fn = (e: GestureResponderEvent) => {
+        e.stopPropagation()
+        fn(props.field)
+      }
+    }
+    return handlePress.current.fn
+  })
+  return (
+    <Controller
+      render={({ field }) => (
+        <MemoWrapper
+          selected={field.value?.id === props.field.id}
+          onWrapperPress={handlePressSetter.current(field.onChange)}
+          htmlId={`field-${props.field.type}-${props.field.id}`}
+          children={content}
+        />
+      )}
+      control={props.control}
+      name="selectedField"
+    />
+  )
+})
+
+NodeSelectorWrapper.displayName = 'NodeSelectorWrapper'
