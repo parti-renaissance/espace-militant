07e61c437dbdcf16a399d60d434a616967f9fc90
Nicolas Guillard <nicolas@nitrique.fr>
2025-03-20 13:51:11 +0100

    fix(lock): fix lock image location and sizing

--- src/features/profil/pages/referrals/components/ReferralLockedCard.tsx
+++ src/features/profil/pages/referrals/components/ReferralLockedCard.tsx
@@ -4,7 +4,8 @@ import { VoxButton } from '@/components/Button'
 import InstanceCard from '@/components/InstanceCard/InstanceCard'
 import { useOpenExternalContent } from '@/hooks/useOpenExternalContent'
 import { HeartHandshake } from '@tamagui/lucide-icons'
-import { Image, View, YStack } from 'tamagui'
+import { Image } from 'expo-image'
+import { View, YStack } from 'tamagui'
 
 export default function ReferralLockedCard({ hideHeader }: { hideHeader?: boolean }) {
   const { isPending, open: openAdh } = useOpenExternalContent({ slug: 'adhesion', utm_campaign: 'profil' })
@@ -13,7 +14,7 @@ export default function ReferralLockedCard({ hideHeader }: { hideHeader?: boolea
     <InstanceCard title="Parrainages" icon={HeartHandshake} hideHeader={hideHeader}>
       <YStack alignItems={'center'} gap={'$8'} justifyContent={'center'}>
         <View margin={'$8'}>
-          <Image source={require('../assets/lock.png')} width={88} height={153} objectFit={'contain'} />
+          <Image source={require('@/assets/illustrations/VisuCadnas.png')} contentFit={'contain'} style={{ height: 153, width: 88 }} />
         </View>
 
         <Text bold>Les parrainages sont réservés aux adhérents. Adhérez pour y participer.</Text>
