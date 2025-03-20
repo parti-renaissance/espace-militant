f49f395ca9f2cc0a3f183d379c946e282f4210fb
Nicolas Guillard <nicolas@nitrique.fr>
2025-03-20 11:39:15 +0100

    feature(referral): add non-member empty state

--- src/features/profil/pages/referrals/components/ReferralCodeCard.tsx
+++ src/features/profil/pages/referrals/components/ReferralCodeCard.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import Text from '@/components/base/Text'
 import InstanceCard from '@/components/InstanceCard/InstanceCard'
 import VoxCard from '@/components/VoxCard/VoxCard'
 import InviteCard from '@/features/profil/pages/referrals/components/InviteCard'
 import ReferralCode from '@/features/profil/pages/referrals/components/ReferralCode'
+import ReferralLockedCard from '@/features/profil/pages/referrals/components/ReferralLockedCard'
+import { useGetProfil } from '@/services/profile/hook'
 import { HeartHandshake } from '@tamagui/lucide-icons'
 import { useMedia } from 'tamagui'
 
@@ -11,6 +13,12 @@ const description = 'Parrainez de nouveaux adhérents qui feront notre force de 
 
 export default function ReferralCodeCard() {
   const { xs } = useMedia()
+  const profil = useGetProfil()
+
+  const isNotMember = useMemo(() => profil.data?.tags?.some((el) => el.code.includes('cotisation_nok')), [profil.data])
+  if (isNotMember) {
+    return <ReferralLockedCard />
+  }
 
   return xs ? (
     <VoxCard>
---
+++ src/features/profil/pages/referrals/components/ReferralLockedCard.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Text from '@/components/base/Text'
+import { VoxButton } from '@/components/Button'
+import InstanceCard from '@/components/InstanceCard/InstanceCard'
+import { useOpenExternalContent } from '@/hooks/useOpenExternalContent'
+import { HeartHandshake } from '@tamagui/lucide-icons'
+import { Image, useMedia, View, YStack } from 'tamagui'
+
+export default function ReferralLockedCard() {
+  const { xs } = useMedia()
+  const { isPending, open: openAdh } = useOpenExternalContent({ slug: 'adhesion', utm_campaign: 'profil' })
+
+  return (
+    <InstanceCard title="Parrainages" icon={HeartHandshake} hideTitle={xs}>
+      <YStack alignItems={'center'} gap={'$8'} justifyContent={'center'}>
+        <View margin={'$8'}>
+          <Image source={require('../assets/lock.png')} width={88} height={153} objectFit={'contain'} />
+        </View>
+
+        <Text bold>Les parrainages sont réservés aux adhérents. Adhérez pour y participer.</Text>
+
+        <VoxButton theme="yellow" size="lg" disabled={isPending} onPress={openAdh()} alignSelf={'center'}>
+          J’adhère
+        </VoxButton>
+      </YStack>
+    </InstanceCard>
+  )
+}
