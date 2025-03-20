fc6a76019be1936e58a89c4b2d5d74386cee550d
Nicolas Guillard <nicolas@nitrique.fr>
2025-03-20 12:04:03 +0100

    fix(referral): remove double scrollview

--- src/features/profil/pages/referrals/components/ReferralCodeCard.tsx
+++ src/features/profil/pages/referrals/components/ReferralCodeCard.tsx
@@ -5,30 +5,26 @@ import VoxCard from '@/components/VoxCard/VoxCard'
 import InviteCard from '@/features/profil/pages/referrals/components/InviteCard'
 import ReferralCode from '@/features/profil/pages/referrals/components/ReferralCode'
 import { HeartHandshake } from '@tamagui/lucide-icons'
-import { ScrollView, useMedia } from 'tamagui'
+import { useMedia } from 'tamagui'
 
 const description = 'Parrainez de nouveaux adhérents qui feront notre force de demain.'
 
 export default function ReferralCodeCard() {
   const { xs } = useMedia()
 
-  return (
-    <ScrollView>
-      {xs ? (
-        <VoxCard>
-          <VoxCard.Content>
-            <Text.SM>{description}</Text.SM>
+  return xs ? (
+    <VoxCard>
+      <VoxCard.Content>
+        <Text.SM>{description}</Text.SM>
 
-            <ReferralCode />
-            <InviteCard />
-          </VoxCard.Content>
-        </VoxCard>
-      ) : (
-        <InstanceCard title="Parrainages" icon={HeartHandshake} description={description}>
-          <ReferralCode />
-          <InviteCard />
-        </InstanceCard>
-      )}
-    </ScrollView>
+        <ReferralCode />
+        <InviteCard />
+      </VoxCard.Content>
+    </VoxCard>
+  ) : (
+    <InstanceCard title="Parrainages" icon={HeartHandshake} description={description}>
+      <ReferralCode />
+      <InviteCard />
+    </InstanceCard>
   )
 }
