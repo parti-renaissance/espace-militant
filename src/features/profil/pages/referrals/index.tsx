0b2f06027a99433546ffcff643a7fbff17c91c46
Nicolas Guillard <nicolas@nitrique.fr>
2025-03-25 15:27:27 +0100

    feature(referral): RE-3133 Referral list base

--- src/features/profil/pages/referrals/index.tsx
+++ src/features/profil/pages/referrals/index.tsx
@@ -1,10 +1,15 @@
 import ScrollView from '@/features/profil/components/ScrollView'
 import ReferralCodeCard from '@/features/profil/pages/referrals/components/ReferralCodeCard'
+import ReferralListCard from '@/features/profil/pages/referrals/components/ReferralListCard'
+import { YStack } from 'tamagui'
 
 export default function ReferralsPage() {
   return (
     <ScrollView>
-      <ReferralCodeCard />
+      <YStack gap={'$8'}>
+        <ReferralCodeCard />
+        <ReferralListCard />
+      </YStack>
     </ScrollView>
   )
 }
