3e85f6fdbfe978b38658679a9d1adcfc25cbaf5e
Nicolas Guillard <nicolas@nitrique.fr>
2025-03-25 16:43:25 +0100

    feature(referral): RE-3318 success modal (#1041)

---
+++ src/features/profil/pages/referrals/components/ReferralSuccess.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Text from '@/components/base/Text'
+import { Image, YStack } from 'tamagui'
+import Button from '../../../../../components/Button'
+
+interface Props {
+  onClose?: () => void
+  name: string
+}
+
+export default function ReferralSuccess({ onClose, name }: Props) {
+  return (
+    <YStack gap={'$8'} padding={'$8'} alignItems={'center'} maxWidth={480}>
+      <Image source={require('@/assets/illustrations/sent.png')} />
+      <Text.LG bold textAlign={'center'}>
+        Et hop,{'\n'}une invitation envoyée à {name} !
+      </Text.LG>
+      <Text textAlign={'center'}>Dans quelques minutes, il lui sera possible d’adhérer depuis un lien contenu dans l’email.</Text>
+      <Text textAlign={'center'}>Il lui sera également possible de signaler une invitation non sollicitée.</Text>
+
+      <Button theme="orange" size="xl" onPress={onClose} alignSelf={'center'}>
+        <Button.Text color="$white1" bold>
+          Fermer
+        </Button.Text>
+      </Button>
+    </YStack>
+  )
+}
