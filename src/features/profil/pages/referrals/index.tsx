cb2c890bc842e5bdbc24e0fc488d29afe17a2a90
Nicolas Guillard <nicolas@nitrique.fr>
2025-04-03 15:42:16 +0200

    fix(referral): RE-3131 don't show list if not adherent

--- src/features/profil/pages/referrals/index.tsx
+++ src/features/profil/pages/referrals/index.tsx
@@ -1,15 +1,25 @@
+import React from 'react'
 import ScrollView from '@/features/profil/components/ScrollView'
 import ReferralCodeCard from '@/features/profil/pages/referrals/components/ReferralCodeCard'
 import ReferralListCard from '@/features/profil/pages/referrals/components/ReferralListCard'
-import { YStack } from 'tamagui'
+import ReferralLockedCard from '@/features/profil/pages/referrals/components/ReferralLockedCard'
+import { useIsAdherent } from '@/services/profile/hook'
+import { useMedia, YStack } from 'tamagui'
 
 export default function ReferralsPage() {
+  const isAdherent = useIsAdherent()
+  const { xs } = useMedia()
+
   return (
     <ScrollView>
-      <YStack gap={'$8'}>
-        <ReferralCodeCard />
-        <ReferralListCard />
-      </YStack>
+      {isAdherent ? (
+        <YStack gap={'$8'}>
+          <ReferralCodeCard />
+          <ReferralListCard />
+        </YStack>
+      ) : (
+        <ReferralLockedCard hideHeader={xs} />
+      )}
     </ScrollView>
   )
 }
