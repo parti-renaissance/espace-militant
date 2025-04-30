b2c4250591d79ad199baec8f3becdd057daf6d2c
Nicolas Guillard <nicolas@nitrique.fr>
2025-04-30 14:19:30 +0200

    fix(notification): RE-3451 deduplicate token

--- src/features/push-notification/hook/useAddPushToken.ts
+++ src/features/push-notification/hook/useAddPushToken.ts
@@ -6,7 +6,6 @@ import { TokenCannotBeSubscribedError } from '../errors'
 export function useAddPushToken() {
   return useMutation({
     mutationFn: async (variables: { token: string }) => {
-      console.log('Query token with', variables)
       return addPushToken({
         identifier: variables.token,
         source: 'vox',
