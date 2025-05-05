a5fc9b17055ef8d7f045725aa82cd0db29ee71c2
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2025-05-05 18:07:46 +0200

    [Live] improve check Vimeo of live_url

--- src/features/events/utils.ts
+++ src/features/events/utils.ts
@@ -123,5 +123,5 @@ export const isEventHasNationalLive = (
     return false
   }
 
-  return event.live_url.startsWith('https://vimeo.com/event/') && Boolean(event.is_national)
+  return event.live_url.startsWith('https://vimeo.com/') && Boolean(event.is_national)
 }
