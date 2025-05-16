4fbc2e3da97eda8337e2c5ade0bf9bf03b39c550
Emilien-Renaissance <emilien.vandevelde@parti-renaissance.fr>
2025-05-16 17:27:41 +0200

    feature(referrals): RE-3387 add referrals score card

--- src/features/profil/pages/referrals-scoreboard/index.tsx
+++ src/features/profil/pages/referrals-scoreboard/index.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import ScrollView from '@/features/profil/components/ScrollView'
 import SkeCard from '@/components/Skeleton/CardSkeleton'
-import { useReferralScoreboard } from '@/services/referral/hook'
+import { useReferralScoreboard, useReferralStatistics } from '@/services/referral/hook'
 import ReferralScoreboardTable from './components/ReferralScoreboardTable'
 import { VoxButton } from '@/components/Button'
 import Text from '@/components/base/Text'
 import { HeartHandshake } from '@tamagui/lucide-icons'
 import VoxCard from '@/components/VoxCard/VoxCard'
 import { Link } from 'expo-router'
+import ReferralScoreCard from './components/ReferralScoreCard'
+import { useGetProfil } from '@/services/profile/hook'
 
 export default function ReferralsScoreboardPage() {
   const { data: scoreboard, isLoading: isLoadingScoreboard } = useReferralScoreboard()
+  const { data: statistics, isLoading: isLoadingStatistics } = useReferralStatistics()
+  const { data: user } = useGetProfil()
 
-  if (isLoadingScoreboard || !scoreboard) {
+  if ((isLoadingScoreboard && isLoadingStatistics) || !scoreboard) {
     return (
       <ScrollView>
         <Skeleton />
@@ -21,14 +25,20 @@ export default function ReferralsScoreboardPage() {
   }
 
   return (
-    <ScrollView>
+    <ScrollView pb='$xxxlarge'>
       <VoxCard gap="$medium" overflow='hidden'>
         <VoxCard.Content>
           <Text.LG bold>Campagne de parrainage</Text.LG>
           <Text.MD>Participez à notre grande campagne de {'\n'}parrainage jusqu’au 15 juillet !</Text.MD>
-          <Link href="/profil/parrainages">
+          <ReferralScoreCard
+            fullName={`${user?.first_name ?? ''} ${user?.last_name ?? ''}`}
+            rank={scoreboard.global_rank}
+            referralCount={statistics?.nb_referral_finished ?? 0}
+            profileImage={user?.image_url}
+          />
+          <Link href="/profil/parrainages" asChild>
             <VoxButton theme='orange' backgroundColor='$orange9' iconLeft={HeartHandshake} width='100%' size='lg'>
-              Faire un premier parrainage
+              Parrainer une nouvelle adhésion
             </VoxButton>
           </Link>
         </VoxCard.Content>
