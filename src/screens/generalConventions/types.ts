c7af95ccd299965424af17bbb54dd624eba96602
Emilien-Renaissance <emilien.vandevelde@parti-renaissance.fr>
2025-07-28 11:22:03 +0200

    feat(general-conventions): delete feature

--- src/screens/generalConventions/types.ts
+++
@@ -1,6 +0,0 @@
-export type GeneralConventionScreenProps = (props: { topVisual: number }) => JSX.Element
-export enum GeneralConventionOrganizerEnum {
-  ASSEMBLY = 'assembly',
-  COMMITTEE = 'committee',
-  DISTRICT = 'district',
-}
