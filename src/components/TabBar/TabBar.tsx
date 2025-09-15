337eb6caec669c5a04a03b43cfb01b12fcb3876f
Emilien-Renaissance <emilien.vandevelde@parti-renaissance.fr>
2025-09-15 10:38:43 +0200

    feat(scan): scope meeting_scanner

--- src/components/TabBar/TabBar.tsx
+++ src/components/TabBar/TabBar.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useMemo, useRef } from 'react'
 import { LayoutChangeEvent, LayoutRectangle, Platform, SafeAreaView as RNSafeAreaView, StyleSheet } from 'react-native'
 import Animated, { interpolate, useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated'
 import { SafeAreaView } from 'react-native-safe-area-context'
@@ -10,6 +10,7 @@ import MoreSheet from './MoreSheet'
 import { TabBarNavProps, TabNavOptions } from './types'
 import clientEnv from '@/config/clientEnv'
 import FutureButton from '../Buttons/FutureButton'
+import { useGetUserScopes } from '@/services/profile/hook'
 
 const SAV = Platform.OS !== 'ios' ? SafeAreaView : RNSafeAreaView
 const SAVProps: any = Platform.OS !== 'ios' ? { edges: ['bottom'] } : {}
@@ -112,6 +113,9 @@ const MemoTab = React.memo(Tab)
 
 const TabBarNav = ({ state, descriptors, navigation, hide }: TabBarNavProps) => {
   const [otherFocus, setOtherFocus] = React.useState(false)
+  const { data: scopes } = useGetUserScopes()
+
+  const hasScannerScope = useMemo(() => scopes?.some((scope) => scope.code === 'meeting_scanner'), [scopes])
 
   const filteredRoutes = React.useMemo(
     () =>
@@ -204,7 +208,7 @@ const TabBarNav = ({ state, descriptors, navigation, hide }: TabBarNavProps) => 
     <>
       <SAV {...SAVProps} style={{ backgroundColor: 'white' }}>
 
-        {clientEnv.ENVIRONMENT === 'staging' ? (
+        {hasScannerScope ? (
           <XStack zIndex={1000} height={58 + 16 + 16} position="absolute" top={-58 - 8 - 16 - 16} right={0} left={0} bottom={0} justifyContent="center" alignItems="center">
             <XStack padding={16} backgroundColor="#290A4299" borderRadius={999}>
               <FutureButton onPress={() => navigation.navigate('scanner')}>
