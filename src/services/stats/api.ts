b782219a8db1fe2c17e6dbda874781f1f50c805d
ivdami <231574100+ivdami@users.noreply.github.com>
2025-12-05 11:40:20 +0100

    feat(event): avoid sending scope for non-creation endpoints

--- src/services/stats/api.ts
+++ src/services/stats/api.ts
@@ -2,21 +2,20 @@ import * as schemas from '@/services/stats/schema'
 import { api } from '@/utils/api'
 import { z } from 'zod'
 
-export const getPublicationStats = (props: { uuid: string; scope: string }) =>
+export const getPublicationStats = (props: { uuid: string }) =>
   api({
     method: 'get',
-    path: `/api/v3/stats/publication/${props.uuid}?scope=${props.scope}`,
+    path: `/api/v3/stats/publication/${props.uuid}`,
     requestSchema: z.void(),
     responseSchema: schemas.RestPublicationStatsResponseSchema,
     type: 'private',
   })()
 
-export const getEventStats = (props: { uuid: string; scope: string }) =>
+export const getEventStats = (props: { uuid: string }) =>
   api({
     method: 'get',
-    path: `/api/v3/stats/event/${props.uuid}?scope=${props.scope}`,
+    path: `/api/v3/stats/event/${props.uuid}`,
     requestSchema: z.void(),
     responseSchema: schemas.RestEventStatsResponseSchema,
     type: 'private',
   })()
-
