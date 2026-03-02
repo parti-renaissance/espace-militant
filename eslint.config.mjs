62571baea5bc7b6e1a7e20890ef1a2e745158ee1
Emilien-Renaissance <emilien.vandevelde@parti-renaissance.fr>
2026-03-02 14:36:00 +0100

    migrate ESLint deps to v10

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,41 @@
+import { defineConfig } from 'eslint/config'
 import eslint from '@eslint/js'
-import reactCompiler from 'eslint-plugin-react-compiler'
+import reactHooks from 'eslint-plugin-react-hooks'
 import unusedImports from 'eslint-plugin-unused-imports'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
-  plugins: {
-    'react-compiler': reactCompiler,
-    'unused-imports': unusedImports,
-  },
-  rules: {
-    'no-console': 'warn',
-    'react-compiler/react-compiler': 'warn',
-    'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
-    '@typescript-eslint/no-unused-vars': 'off',
-    'unused-imports/no-unused-imports': 'error',
-    'unused-imports/no-unused-vars': [
-      'warn',
-      {
-        vars: 'all',
-        varsIgnorePattern: '^_',
-        args: 'after-used',
-        argsIgnorePattern: '^_',
-      },
+export default defineConfig(
+  {
+    ignores: [
+      'dist/**',
+      '**/node_modules/**',
+      'android/app/build/**',
+      'ios/build/**',
+      '.expo/**',
+      '**/mapbox-gl*.js',
     ],
   },
-})
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  reactHooks.configs.flat.recommended,
+  {
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      'no-console': 'warn',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+)
