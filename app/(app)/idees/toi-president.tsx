7783ee626df1384b9c4ca950bafbf3235203ea1b
Alex-renaissance <alexandre.damasceno@parti-renaissance.fr>
2026-06-02 19:13:56 +0200

    feat(BOT/JEU): improvement in bot and game toi president

--- app/(app)/idees/toi-president.tsx
+++ app/(app)/idees/toi-president.tsx
@@ -3,18 +3,31 @@ import { Redirect } from 'expo-router'
 import Head from 'expo-router/head'
 import { Dices } from '@tamagui/lucide-icons'
 
+import { AccessDeny } from '@/components/AccessDeny'
 import Header from '@/components/AppStructure/Header'
 import Layout from '@/components/AppStructure/Layout/Layout'
 import ToiPresidentGameScreen from '@/features_next/ideas/toiPresident/page'
 
 import * as metatags from '@/config/metatags'
 import { useSession } from '@/ctx/SessionProvider'
+import { useUserScopeFeatures } from '@/services/profile/hook'
+import { FEATURES } from '@/utils/Scopes'
 
 export default function ToiPresidentRoute() {
   const { isAuth } = useSession()
+  const { hasFeature, isLoading } = useUserScopeFeatures({ enabled: isAuth })
+  const canAccess = hasFeature(FEATURES.AI_ANTISECHE)
 
   if (!isAuth) {
     return <Redirect href="/evenements" />
+  }
+
+  if (!canAccess && !isLoading) {
+    return (
+      <Layout.Container hideSideBar hideTabBar>
+        <AccessDeny />
+      </Layout.Container>
+    )
   }
 
   return (
