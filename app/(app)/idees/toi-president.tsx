f58f188e5170da0fc8d0bc169a1f7669f85899b7
Emilien-Renaissance <emilien.vandevelde@parti-renaissance.fr>
2026-06-05 11:59:31 +0200

    fix(nav): disable swipe back on Toi président game

--- app/(app)/idees/toi-president.tsx
+++ app/(app)/idees/toi-president.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Stack as RouterStack } from 'expo-router'
 import Head from 'expo-router/head'
 import { Dices } from '@tamagui/lucide-icons'
 
@@ -12,11 +13,12 @@ import * as metatags from '@/config/metatags'
 export default function ToiPresidentRoute() {
   return (
     <RequireAuth>
+      <RouterStack.Screen options={{ gestureEnabled: false, fullScreenGestureEnabled: false }} />
       <Head>
         <title>{metatags.createTitle('Toi président')}</title>
       </Head>
       <Header title="Toi président" icon={Dices} navigation={{ showBackButton: true }} style={{ showOn: 'sm' }} />
-      <Layout.Container safeHorizontalPadding={false} hideTabBar>
+      <Layout.Container hideTabBar>
         <ToiPresidentGameScreen />
       </Layout.Container>
     </RequireAuth>
