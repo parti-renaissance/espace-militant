bf97947b39e522658e6e43d768301bedb20124cf
Emilien-Renaissance <emilien.vandevelde@parti-renaissance.fr>
2026-06-09 15:03:35 +0200

    fix(onboard): redirect authenticated users from signup screens

--- src/features_next/signup/pages/SignupInscriptionScreen/index.tsx
+++ src/features_next/signup/pages/SignupInscriptionScreen/index.tsx
@@ -1,10 +1,16 @@
+import { Redirect } from 'expo-router'
 import { useMedia } from 'tamagui'
 
+import { useUnauthGuard } from '@/features_next/signup/hooks/useUnauthGuard'
 import SignupInscriptionDesktopScreen from '@/features_next/signup/pages/SignupInscriptionScreen/SignupInscriptionDesktopScreen'
 import SignupInscriptionMobileScreen from '@/features_next/signup/pages/SignupInscriptionScreen/SignupInscriptionMobileScreen'
 
 export default function SignupInscriptionScreen() {
   const media = useMedia()
+  const { isWaiting, shouldRedirect } = useUnauthGuard()
+
+  if (isWaiting) return null
+  if (shouldRedirect) return <Redirect href="/" />
 
   return media.gtSm ? <SignupInscriptionDesktopScreen /> : <SignupInscriptionMobileScreen />
 }
